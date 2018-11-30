42bf3c30b81f9e6dc75f435b1930f128ac478f25
Justin <sips96041016@gmail.com>
2018-11-30 20:22:06 +0800

    2nd edition

--- project2.cpp
+++ project2.cpp
@@ -3,9 +3,26 @@
 #include <queue>
 #include <stack>
 #include <utility>
+#include <algorithm>
 #include <iomanip>//setw
-#define Max(a,b) (a>b)?a:b
+#define triple(x,y,z) make_pair(x,make_pair(y,z))
 using namespace std;
+
+struct greater_triple{
+  bool operator()(pair<int,pair<int,int>>& a,pair<int,pair<int,int>>& b) const{
+    return a>b;//> or >=
+  }
+};
+class DIR{
+public:
+    DIR(int total_visit=0, int direction=-1){ tot_visit = total_visit; dir = direction; }
+    void operator= (DIR f){ tot_visit = f.tot_visit; dir = f.dir; }
+    bool operator== (DIR f){ return( tot_visit == f.tot_visit && dir == f.dir ); }
+    bool operator!= (DIR f){ return !(operator==(f)); }
+    int tot_visit;
+    int dir;
+};
+const DIR unknown,start_point(0,4);
 //1 1 1
 //1 R 1
 //1 1 1
@@ -51,27 +68,54 @@ void bfs(int **dist,char **dir,bool **visited,int x,int y,int n,int m){
 
 void get_stk_top(int &x,int &y,int k){ x = stk[k].top(); stk[k].pop(); y = stk[k].top(); stk[k].pop(); }
 
-bool check_and_visit(int **dist,char **dir, stack<pair<int,int>> &p_stk, int &x, int &y, int a, int b){
-    if( dist[a][b]!=-1 && dist[a][b]<dist[x][y] && ( dir[a][b]=='o' || dir[a][b]==dir[x][y] ) )
-        { x=a; y=b; p_stk.push(make_pair(x,y)); return true; }
-    return false;
+void update_from(int **dist, char **dir, bool **visited, DIR **from, vector<pair<int,pair<int,int>>> &heap_f,
+                 int x, int y, int a, int b, int from_dir){
+    if( dist[a][b]!=-1 && dist[a][b]<dist[x][y] && ( dir[a][b]=='o' || dir[a][b]==dir[x][y] ) && from[a][b]==unknown ){
+        int v = from[a][b].tot_visit = from[x][y].tot_visit + visited[a][b];
+        from[a][b].dir = from_dir;
+        heap_f.push_back(triple(v,a,b)); push_heap(heap_f.begin(),heap_f.end(),greater_triple());
+    }
 }
 
-stack<pair<int,int>> from_R(int **dist, char **dir, bool **visited, int x, int y){
-    stack<pair<int,int>> p_stk;
-    while(dist[x][y]>1){
-        visited[x][y] = true;
-        if(check_and_visit(dist,dir,p_stk,x,y,x-1,y)) continue;//u
-        if(check_and_visit(dist,dir,p_stk,x,y,x+1,y)) continue;//d
-        if(check_and_visit(dist,dir,p_stk,x,y,x,y-1)) continue;//l
-        if(check_and_visit(dist,dir,p_stk,x,y,x,y+1)) continue;//r
+vector<pair<int,int>> from_R(int **dist, char **dir, bool **visited, DIR **from, int R_x, int R_y, int x, int y, int n, int m){
+    vector<pair<int,int>> path;
+    vector<pair<int,pair<int,int>>> heap_f;// I want priority queue
+    pair<int,pair<int,int>> block;
+    int v,i,j;
+    for(i=1;i<=n;i++) for(j=1;j<=m;j++) from[i][j] = unknown;
+    heap_f.push_back(triple(0,x,y)); from[x][y] = start_point;//start point
+    while(!heap_f.empty()){//will never use this condition
+        block = heap_f.front(); v = block.first; x = block.second.first; y = block.second.second;
+        pop_heap(heap_f.begin(),heap_f.end(),greater_triple()); heap_f.pop_back();
+        update_from(dist,dir,visited,from,heap_f,x,y,x-1,y,1);//u ,from d
+        update_from(dist,dir,visited,from,heap_f,x,y,x+1,y,0);//d ,from u
+        update_from(dist,dir,visited,from,heap_f,x,y,x,y-1,3);//l ,from r
+        update_from(dist,dir,visited,from,heap_f,x,y,x,y+1,2);//r ,from l
+        if(from[R_x][R_y]!=unknown) break;
     }
-    return p_stk;
+    x = R_x; y = R_y;
+    while(1){
+        if( from[x][y].dir==0 ) x--;
+        else if( from[x][y].dir==1 ) x++;
+        else if( from[x][y].dir==2 ) y--;
+        else if( from[x][y].dir==3 ) y++;
+        else { cout<<from[x][y].dir<<" "<<x<<" "<<y<<" noooooo!!!!!!!"<<endl; break;}
+        if( from[x][y] != start_point ) { path.push_back(make_pair(x,y)); visited[x][y] = true; }
+        else break;
+    }
+    return path;
 }
 
 int go_go(int **dist, char **dir, bool **visited, int R_x, int R_y, int n, int m, int max_dist){
     int i,j,k,x,y,total = 0;
-    stack<pair<int,int>> go_p_stk, back_p_stk;
+
+    DIR **from = new DIR*[n+2];
+    DIR *tmpf = new DIR[(n+2)*(m+2)];
+    for(i=0;i<n+2;i++) from[i] = &(tmpf[i*(m+2)]);
+
+    vector<pair<int,int>> go_path, back_path;
+    vector<pair<int,int>>::iterator it;
+    vector<pair<int,int>>::reverse_iterator rit;
     char direction = 'u';
     for(i=1;i<=n;i++) for(j=1;j<=m;j++) visited[i][j] = false;
     visited[0][0] = true;
@@ -82,21 +126,20 @@ int go_go(int **dist, char **dir, bool **visited, int R_x, int R_y, int n, int m
         while ( visited[x][y] && !stk[k].empty() ) get_stk_top(x,y,k);
         if(stk[k].empty()) break;
         //from R to (x,y)
-        go_p_stk = from_R(dist,dir,visited,x,y);
-        while(!go_p_stk.empty()){
-            cout<<go_p_stk.top().first<<" "<<go_p_stk.top().second<<endl;
-            back_p_stk.push(go_p_stk.top());
-            go_p_stk.pop();
+        go_path = from_R(dist,dir,visited,from,R_x,R_y,x,y,n,m);
+        for (it = go_path.begin() ; it != go_path.end(); ++it){
+            cout<<it->first<<" "<<it->second<<endl;
             total++;
         }
         cout<<x<<" "<<y<<"       0000000000000"<<endl;
-        while(!back_p_stk.empty()){
-            cout<<back_p_stk.top().first<<" "<<back_p_stk.top().second<<endl;
-            back_p_stk.pop();
+        back_path = from_R(dist,dir,visited,from,R_x,R_y,x,y,n,m);
+        for (rit = back_path.rbegin() ; rit != back_path.rend(); ++rit){
+            cout<<rit->first<<" "<<rit->second<<endl;
             total++;
         }
         cout<<R_x<<" "<<R_y<<"       11111111111111"<<endl;
     }
+    delete [] tmpf; delete [] from;
     return total;
 }
 
@@ -104,16 +147,13 @@ int main(void){
     int i,j,n,m,k,max_dist,R_x,R_y;
     char c;
     char **dir,*tmpc;
-    int **dist,*tmp;
     bool **visited,*tmpb;
+    int **dist,*tmp;
     //input
     fstream fs;
     fs.open ("floor_3.data", ios::in );
     fs>>n>>m>>max_dist;
 
-    dist = new int*[n+2];
-    tmp = new int[(n+2)*(m+2)];
-    for(i=0;i<n+2;i++) dist[i] = &(tmp[i*(m+2)]);
     dir = new char*[n+2];
     tmpc = new char[(n+2)*(m+2)];
     for(i=0;i<n+2;i++) dir[i] = &(tmpc[i*(m+2)]);
@@ -121,6 +161,9 @@ int main(void){
     visited = new bool*[n+2];
     tmpb = new bool[(n+2)*(m+2)];
     for(i=0;i<n+2;i++) visited[i] = &(tmpb[i*(m+2)]);
+    dist = new int*[n+2];
+    tmp = new int[(n+2)*(m+2)];
+    for(i=0;i<n+2;i++) dist[i] = &(tmp[i*(m+2)]);
 
     for(j=1;j<=m;j++) { dist[0][j] = -1; dist[n+1][j] = -1; }
     for(i=1;i<=n;i++) {
